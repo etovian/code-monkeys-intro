5dd7b2b690700ab20abd619ae05c7bd7d7116ea9
Mike Green <mike.green@stgconsulting.com>
2015-05-20 22:31:10 -0600

    Added route for /home and set it as default

--- js/route.config.js
+++ js/route.config.js
@@ -6,28 +6,13 @@
 	
 	function config($routeProvider) {
 		$routeProvider
-			.when("/initiative-list", {
-				templateUrl: "views/templates/initiative-list.html",
-				controller: "InitiativeListController",
+			.when("/home", {
+				templateUrl: "views/templates/home.html",
+				controller: "HomeController",
 				controllerAs: "list"
 			})
-			.when("/manage-characters", {
-				templateUrl: "views/templates/manage-characters.html",
-				controller: "ManageCharactersController",
-				controllerAs: "pcs"
-			})
-			.when("/npcs", {
-				templateUrl: "views/templates/manage-npc-templates.html",
-				controller: "ManageNpcTemplatesController",
-				controllerAs: "npcs"
-			})
-			.when("/admin", {
-				templateUrl: "views/templates/admin.html",
-				controller: "AdminController",
-				controllerAs: "admin"
-			})
 			.otherwise({
-				redirectTo: "/initiative-list"
+				redirectTo: "/home"
 			});
 	};
 	
