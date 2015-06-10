c3898d0a404664450f586118d9da76d3ae6fa5c1
Wendy <whgoller@gmail.com>
2015-06-09 19:45:07 -0600

    Rewrote controller to not use the evil $scope

--- js/controllers/wendy-goller.controller.js
+++ js/controllers/wendy-goller.controller.js
@@ -1,27 +1,38 @@
-var app = angular.module('app');
+(function() {
+    'use strict';
 
-app.controller("WendyGollerController", function($scope, bookService){
-    $scope.test = "Hello There!"
+    angular
+        .module('app')
+        .controller('WendyGollerController', WendyGollerController);
     
+    WendyGollerController.$inject = ['bookService'];
+
+    function WendyGollerController(bookService) { 
+        var vm = this;
+        
+        vm.addBook = addBook;
+        vm.deleteBook = deleteBook;
+        vm.getBooksList = getBooksList();
+        vm.newBook = {};
+        vm.books = [];
+        
+        function addBook(){
+            newBook.title = vm.bookTitle;
+            newBook.authorName = vm.authorName;
+            newBook.publishDate = vm.publishDate
+            bookService.addData(newBook);
+        }
     
-    $scope.addBook = function(){
-        var newBook = {};
-        newBook.title = $scope.bookTitle;
-        newBook.authorName = $scope.authorName;
-        newBook.publishDate = $scope.publishDate
-        bookService.addData(newBook);
+        function deleteBook(selectedBook){
+            bookService.removeData(selectedBook);
+        }
+
+        function getBooksList(){
+            bookService.getBooks().then(function(data){
+                console.log(data.data);
+                vm.books = data.data;
+            });
+        }
+
     }
-    
-    $scope.deleteBook = function(selectedBook){
-        bookService.removeData(selectedBook);
-    }
-    
-    $scope.getBooksList = function(){
-        bookService.getBooks().then(function(data){
-            console.log(data.data);
-            $scope.books = data.data;
-        });
-    }();
-    
-    
-});
+})();
